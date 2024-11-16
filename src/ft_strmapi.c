53533b594c8c2578396354fecb0dbcb58ce1b563
artzkaizen <arbeitchuka@gmail.com>
2024-11-16 19:20:01 +0100

    inplement ft_strmapi

--- src/ft_strmapi.c
+++ src/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: chuezeri <chuezeri@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 20:01:09 by chuezeri          #+#    #+#             */
-/*   Updated: 2024/11/14 20:02:09 by chuezeri         ###   ########.fr       */
+/*   Updated: 2024/11/16 19:14:24 by chuezeri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	return (NULL);
+	size_t			len;
+	char			*new_str;
+	unsigned int	i;
+
+	i = 0;
+	len = ft_strlen(s);
+	new_str = (char *)malloc(sizeof(char) * (len));
+	if (!new_str)
+		return (NULL);
+	while (i < len)
+	{
+		new_str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	return (new_str);
 }
